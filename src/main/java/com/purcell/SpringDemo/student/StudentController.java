5ae14b8dc098afb034be116e6328ec27da6c6d20
Iampurcell <iampurcell9@gmail.com>
2025-03-04 17:26:09 +0300

    Building our First sample APi

--- src/main/java/com/purcell/SpringDemo/student/StudentController.java
+++ src/main/java/com/purcell/SpringDemo/student/StudentController.java
@@ -1,4 +1,20 @@
 package com.purcell.SpringDemo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //it tells Spring that this class will handle HTTP requests and return JSON responses
+@RequestMapping("/api/v1/students") //define which URL this controller should respond to and map HTTP requests to specific handler methods.
 public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudent(){
+        return List.of(
+                "Purcell",
+                "Hello world"
+        );
+    }
 }
